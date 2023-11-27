a2cf064faee6ee113a9aa95c923423459a37c7d9
Arkadiusz Kawałko <kawalko@woblink.com>
2023-11-27 16:02:55 +0100

    Adds example file with MySQL table structure

---
+++ examples/db_structure.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `user` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `email` varchar(191) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+    `password` varchar(191) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+    `active` tinyint(3) unsigned DEFAULT NULL,
+    `is_admin` tinyint(3) unsigned DEFAULT NULL,
+    `_seeded` tinyint(3) unsigned DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
