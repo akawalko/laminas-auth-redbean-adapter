b8c80c8c83bf5d7e1bd00e25b14b35b4cc07235d
Arkadiusz Kawałko <kawalko@woblink.com>
2023-11-28 13:30:31 +0100

    Renames sql file and removes seeded column

--- examples/db_structure_mysql.sql
+++ examples/db_structure_mysql.sql
@@ -4,6 +4,5 @@ CREATE TABLE IF NOT EXISTS `user` (
     `password` varchar(191) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
     `is_active` tinyint(3) unsigned DEFAULT 0,
     `is_admin` tinyint(3) unsigned DEFAULT 0,
-    `_seeded` tinyint(3) unsigned DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
